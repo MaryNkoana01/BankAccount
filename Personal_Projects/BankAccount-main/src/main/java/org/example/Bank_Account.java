ae6657965a4ef73340e19f1e99bec0413d25e8a8
mankoan022 <mankoana022@student.wethinkcode.co.za>
2023-08-29 17:20:05 +0200

    added the withdraw method

--- Personal_Projects/BankAccount-main/src/main/java/org/example/Bank_Account.java
+++ Personal_Projects/BankAccount-main/src/main/java/org/example/Bank_Account.java
@@ -13,12 +13,27 @@ public class Bank_Account {
         this.amount = amount;
     }
 
-    public void display(){
-        System.out.println(accountNumber +" "+ customerName + " " + accountType + " " + " " + amount);
-    }
     public void deposit(float deposit){
         amount = amount + deposit;
         System.out.println("Deposited Amount: "+ deposit);
+    }
+    public void withdraw(float withdrawalAmount){
+        if(amount > withdrawalAmount){
+            amount = amount - withdrawalAmount;
+            System.out.println("Amount withdrawn: "+ withdrawalAmount);
+            System.out.println("Balance amount: "+ amount);
+        }
+        else{
+            System.out.println("Insufficient Amount, available amount: "+ amount);
+        }
+
+    }
+    public void checkBalance(){
+        System.out.println("Current balance is: "+ amount);
+    }
+
+    public void display(){
+        System.out.println(accountNumber +" "+ customerName + " " + accountType + " " + " " + amount);
     }
 
 
@@ -27,7 +42,8 @@ public class Bank_Account {
         b.accountDetails(123456789,"Mary","Savings",2000);
         b.deposit(100);
         b.display();
-
+        b.withdraw(500);
+        b.checkBalance();
 
     }
 }
