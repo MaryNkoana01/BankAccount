9ee75712d98a9e6163f8abf137bd8352f93cc8f0
mankoan022 <mankoana022@student.wethinkcode.co.za>
2023-08-31 21:03:09 +0200

    Created Email class and Initiated variables

---
+++ Personal_Projects/EmailGenerator/src/main/java/org/example/Email.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private String email;
+
+
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
